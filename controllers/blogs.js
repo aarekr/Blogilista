ba130f7ee47e61c765105bfc6deee5f1ad45dc23
aarekr <aare212@hotmail.com>
2022-10-22 17:21:02 +0300

    Tehtävä 4.2 step2

---
+++ controllers/blogs.js
@@ -0,0 +1,53 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (req, res) => {
+  Blog.find({}).then(blogs => {
+    res.json(blogs)
+  })
+})
+
+blogsRouter.get('/:id', (req, res, next) => {
+  Blog.findById(req.params.id)
+    .then(blog => {
+      if (blog) {
+        res.json(blog)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post('/', (req, res) => {
+  const body = req.body
+  console.log("postin body:", body)
+  if (!body.author) {
+    return res.status(400).json({ error: 'author missing' })
+  }
+  if (!body.title) {
+    return res.status(400).json({ error: 'title missing' })
+  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: 'http://blogi',
+    likes: 5,
+  })
+  console.log("blog:", blog)
+  blog.save().then(savedBlog => {
+    console.log('blog saved!')
+    res.json(savedBlog)
+  })
+  .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (req, res, next) => {
+  Blog.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
--- index.js
+++ index.js
@@ -1,73 +1,10 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
+const app = require('./app') // varsinainen Express-sovellus
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
+const server = http.createServer(app)
 
-// <password>
-const url = `mongodb+srv://fullstack:<password>@cluster0.vhby0zp.mongodb.net/blogilista?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello Blogilista</h1>')
-})
-
-app.get('/api/blogs', (req, res) => {
-  Blog.find({}).then(blogs => {
-    res.json(blogs)
-  })
-})
-
-app.get('/api/blogs/:id', (req, res) => {
-  Blog.findById(req.params.id).then(blog => {
-    res.json(blog)
-  })
-})
-
-app.post('/api/blogs', (req, res) => {
-    const body = req.body
-    console.log("postin body:", body)
-    if (!body.author) {
-      return res.status(400).json({ error: 'author missing' })
-    }
-    if (!body.title) {
-      return res.status(400).json({ error: 'title missing' })
-    }
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: 'http://blogi',
-      likes: 5,
-    })
-    console.log("blog:", blog)
-    blog.save().then(savedBlog => {
-      console.log('blog saved!')
-      res.json(savedBlog)
-    })
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
