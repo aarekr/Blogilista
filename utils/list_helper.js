479eb16edf187122a392e13a2dd3373415cf4aca
aarekr <aare212@hotmail.com>
2022-10-23 11:00:00 +0300

    Tehtävä 4.5* step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -16,14 +16,33 @@ const firstBlogLikes = (blog) => {
 
 const allLikesPositive = (blogs) => {
   let allPositive = true
-  for (let i = 0; i < blogs.length; i++) {
-    if (blogs[i]['likes'] < 0) {
+  blogs.map(blog => {
+    if (blog['likes'] < 0)
       allPositive = false
-    }
-  }
+  })
   return allPositive
 }
 
+const favoriteBlog = (blogs) => {
+  let tyk_lkm = 0
+  let palautettava = {
+    title: '',
+    author: '',
+    likes: 0
+  }
+  blogs.map(blog => {
+    if (blog['likes'] > tyk_lkm) {
+      tyk_lkm = blog['likes']
+      palautettava = {
+        title: blog['title'],
+        author: blog['author'],
+        likes: blog['likes']
+      }
+    }
+  })
+  return palautettava
+}
+
 module.exports = {
-  dummy, totalLikes, firstBlogLikes, allLikesPositive
+  dummy, totalLikes, firstBlogLikes, allLikesPositive, favoriteBlog
 }
