05ca9ffc220e5031536ed087ed42528ad0f28a12
aarekr <aare212@hotmail.com>
2022-10-24 10:25:44 +0300

    Tehtävä 4.8 step1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs= [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are three blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(3)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('Dijkstra`s blog is included', async () => {
+  const response = await api.get('/api/blogs')
+  const authors = response.body.map(r => r.author)
+  expect(authors).toContainEqual('Edsger W. Dijkstra')
+})
+
+test('there is a blog about React', async () => {
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContainEqual('React patterns')})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
